861f1716cc1304ce45492fa27392a33b3d6d8258
Aditya Vadrevu <adityavadrevu92@gmail.com>
2024-09-01 19:56:29 -0700

    added co-variant inheritance java file

---
+++ src/test/inheritance/PracticeInheritance2.java
@@ -0,0 +1,60 @@
+package test.inheritance;
+
+class Plane
+{
+	
+}
+
+class FighterPlane extends Plane
+{
+	
+}
+
+class Animal
+{
+    public void eat(String foodType)
+    {
+        System.out.println("Animals are required to eat");
+    }
+
+    public void sleep()
+    {
+        System.out.println("Animal is sleeping");
+    }
+
+    public Plane runnig()
+    {
+        System.out.println("Amimal is running");
+        Plane p = new Plane();
+        return p;
+    }
+}
+
+class Tiger extends Animal 
+{
+    public void eat(String foodType)
+	{
+		System.out.println("Tiger hunts and eat");
+	}
+	
+	public void eat()//its overloaded method of Child class and hence its specialzed method of child class
+	{
+		System.out.println("Tiger hunts and eat");
+
+	}
+    public FighterPlane running()//co-varient return type are such return where there is is-A relationship
+	{
+		System.out.println("Tiger is running to hunt");
+		FighterPlane fp=new FighterPlane();
+		return fp;
+	}
+}
+
+public class PracticeInheritance2 {
+    public static void main(String[] args) {
+        
+        Tiger t=new Tiger();
+		t.eat();
+    }
+
+}
