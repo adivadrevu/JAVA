e2451543accbdf0ad06207766ef8ef1243a7a7ca
Aditya Vadrevu <adityavadrevu92@gmail.com>
2024-09-16 22:22:39 -0700

    deleted unwanted files

---
+++ practice/Strings/src/LaunchStringPractice.java
@@ -0,0 +1,17 @@
+public class LaunchStringPractice 
+{
+    public static void main(String[] args) 
+    {
+        String s1="Hello"; //String pool
+        String s2="Hello"; //String pool
+        String s3=new String("Hello"); //Heap
+        String s4=new String("Hello"); //Heap
+        System.out.println(s1==s2); //true
+        System.out.println(s1==s3); //false
+        System.out.println(s3==s4); //false
+        System.out.println(s1.equals(s2)); //true
+        System.out.println(s1.equals(s3)); //true
+        System.out.println(s3.equals(s4)); //true
+    }
+    
+}
