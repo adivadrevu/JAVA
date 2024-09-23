8540152a508777a4d8f4d4402165d73968f6c29f
Aditya Vadrevu <adityavadrevu92@gmail.com>
2024-09-22 22:50:46 -0700

    added ArrayList.java

---
+++ practice/collections/PracticeArrayList.java
@@ -0,0 +1,34 @@
+package practice.collections;
+
+import java.util.ArrayList;
+public class PracticeArrayList 
+{
+
+	public static void main(String[] args) 
+	{
+		ArrayList al=new ArrayList();
+		
+		al.add(100);
+		al.add(200);
+		al.add(44.5);
+		al.add("Anil");
+		al.add('t');
+		al.add(true);
+		
+		//System.out.println(al);
+		
+		ArrayList al2=new ArrayList();
+		al2.add(10);
+		al2.add(20);
+		al2.add(0, 44);
+		al2.addAll(al);
+		System.out.println(al2);
+		System.out.println(al2.contains(20));
+		System.out.println(al2.containsAll(al));
+		al2.add(10);
+		System.out.println(al2);
+		
+
+	}
+
+}
