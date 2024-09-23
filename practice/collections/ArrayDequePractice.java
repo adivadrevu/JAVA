fd04a70427e90e755bc9989d3b821534f1a44750
Aditya Vadrevu <adityavadrevu92@gmail.com>
2024-09-22 22:48:46 -0700

    added collections practice file

---
+++ practice/collections/ArrayDequePractice.java
@@ -0,0 +1,23 @@
+package practice.collections;
+
+import java.util.ArrayDeque;
+
+public class ArrayDequePractice 
+{
+
+	public static void main(String[] args) 
+	{
+		ArrayDeque ad=new ArrayDeque();
+		ad.add(10);
+		ad.add(20);
+		ad.add(30);
+		ad.add(40);
+		System.out.println(ad);
+		ad.add(40);
+		System.out.println(ad);
+		
+		
+
+	}
+
+}
