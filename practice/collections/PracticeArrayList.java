1fb6ec4d45cbbea0726a6e231e47bba555b58385
Aditya Vadrevu <adityavadrevu92@gmail.com>
2024-09-24 13:41:57 -0700

    added new files

--- practice/collections/PracticeArrayList.java
+++ practice/collections/PracticeArrayList.java
@@ -14,22 +14,23 @@ public class PracticeArrayList
 		al.add(44.5);
 		al.add("Anil");
 		al.add('t');
+		al.add(null); 
 		// al.add(true);
 		
 		System.out.println(al);
 		
-		ArrayList al2=new ArrayList();
-		al2.add(10);
-		al2.add(20);
-		al2.contains(10); //t
-		al2.add(0, 44);
-		al2.add(0, 45);
-		al2.addAll(al);
+		// ArrayList al2=new ArrayList();
+		// al2.add(10);
+		// al2.add(20);
+		// al2.contains(10); //t
+		// al2.add(0, 44);
+		// al2.add(0, 45);
+		// al2.addAll(al);
 		// System.out.println(al2);
 		// System.out.println(al2.contains(20));
-		// System.out.println(al2.containsAll(al));
-		// al2.add(10);
-		System.out.println(al2);
+		// // System.out.println(al2.containsAll(al));
+		// // al2.add(10);
+		// System.out.println(al2);
 		
 
 	}
---
+++ practice/collections/PracticePriorityQue.java
@@ -0,0 +1,22 @@
+package practice.collections;
+
+import java.util.PriorityQueue;
+
+public class PracticePriorityQue {
+    public static void main(String[] args) {
+        
+        PriorityQueue pq = new PriorityQueue();
+        pq.add(100);
+        pq.add(50);
+        pq.add(75);
+        pq.add(25);
+        pq.add(125);
+        pq.add(150);
+        pq.add(175);
+        pq.add(200);
+
+        System.out.println(pq);
+    }
+
+
+}
