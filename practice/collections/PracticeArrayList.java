504afba13f680a51f9913369099763da030ef377
Aditya Vadrevu <adityavadrevu92@gmail.com>
2024-09-23 17:13:29 -0700

    practiced few methods in Collections

--- practice/collections/PracticeArrayList.java
+++ practice/collections/PracticeArrayList.java
@@ -9,23 +9,26 @@ public class PracticeArrayList
 		ArrayList al=new ArrayList();
 		
 		al.add(100);
+		al.add(100);
 		al.add(200);
 		al.add(44.5);
 		al.add("Anil");
 		al.add('t');
-		al.add(true);
+		// al.add(true);
 		
-		//System.out.println(al);
+		System.out.println(al);
 		
 		ArrayList al2=new ArrayList();
 		al2.add(10);
 		al2.add(20);
+		al2.contains(10); //t
 		al2.add(0, 44);
+		al2.add(0, 45);
 		al2.addAll(al);
-		System.out.println(al2);
-		System.out.println(al2.contains(20));
-		System.out.println(al2.containsAll(al));
-		al2.add(10);
+		// System.out.println(al2);
+		// System.out.println(al2.contains(20));
+		// System.out.println(al2.containsAll(al));
+		// al2.add(10);
 		System.out.println(al2);
 		
 
--- practice/collections/PracticeLinkedList.java
+++ practice/collections/PracticeLinkedList.java
@@ -13,23 +13,39 @@ public class PracticeLinkedList
 		ll1.add("Java");
 		ll1.add("Bengaluru");
 		ll1.add(20);
+		System.out.println("*****************");
 		System.out.println(ll1);
+
 		ll1.addFirst(44);
 		ll1.addLast(54);
 		ll1.add(2, "Telusko");
+		System.out.println("*****************");
 		System.out.println(ll1);
+		
 		ll1.offer("DevOps");
 		System.out.println(ll1);
 		ll1.offer("Junit5");
 		System.out.println(ll1);
+		System.out.println("*****************");
 		ll1.offerFirst("SpringBoot");
+		
 		System.out.println(ll1);
-		System.out.println(ll1.peek());
 		System.out.println(ll1);
-		System.out.println(ll1.poll());
-		System.out.println(ll1);
-		ll1.add("Telusko");
-		System.out.println(ll1);
+		System.out.println(ll1.peek() + " peek method"); // SpringBoot
+		
+		System.out.println(ll1); // SpringBoot, 44, Java, Bengaluru, Telusko, 20, DevOps, Junit5, 54
+		
+		System.out.println(ll1.poll()); // SpringBoot
+		
+		System.out.println(ll1); // 44, Java, Bengaluru, Telusko, 20, DevOps, Junit5, 54
+		ll1.add("Telusko"); // 44, Java, Bengaluru, Telusko, 20, DevOps, Junit5, 54, Telusko
+		System.out.println(ll1); // 44, Java, Bengaluru, Telusko, 20, DevOps, Junit5, 54, Telusko
+		
+		ll1.push(200); 
+		System.out.println(ll1); // 200, 44, Java, Bengaluru, Telusko, 20, DevOps, Junit5, 54, Telusko
+
+		System.out.println(ll1.pop()); // 200
+		System.out.println(ll1); // Java, Bengaluru, Telusko, 20, DevOps, Junit5, 54, Telusko
 
 	}
 
